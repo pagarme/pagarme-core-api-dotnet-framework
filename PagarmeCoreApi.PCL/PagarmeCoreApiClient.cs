c151712cf055d18ae96371d284e13ee25e5674a0
Magno <magnomoreira@yahoo.com>
2023-06-22 14:42:29 -0300

    5.7.5 (#26)
    Co-authored-by: Unknown <samuel.leite@pagar.me>

--- PagarmeCoreApi.PCL/PagarmeCoreApiClient.cs
+++ PagarmeCoreApi.PCL/PagarmeCoreApiClient.cs
@@ -102,17 +102,6 @@ namespace PagarmeCoreApi.PCL
         }
 
         /// <summary>
-        /// Singleton access to Transactions controller
-        /// </summary>
-        public ITransactionsController Transactions
-        {
-            get
-            {
-                return TransactionsController.Instance;
-            }
-        }
-
-        /// <summary>
         /// Singleton access to Transfers controller
         /// </summary>
         public ITransfersController Transfers
@@ -120,6 +109,17 @@ namespace PagarmeCoreApi.PCL
             get
             {
                 return TransfersController.Instance;
+            }
+        }
+
+        /// <summary>
+        /// Singleton access to Transactions controller
+        /// </summary>
+        public ITransactionsController Transactions
+        {
+            get
+            {
+                return TransactionsController.Instance;
             }
         }
         /// <summary>
@@ -145,8 +145,9 @@ namespace PagarmeCoreApi.PCL
         /// <summary>
         /// Client initialization constructor
         /// </summary>
-        public PagarmeCoreApiClient(string basicAuthUserName, string basicAuthPassword)
+        public PagarmeCoreApiClient(string serviceRefererName, string basicAuthUserName, string basicAuthPassword)
         {
+            Configuration.ServiceRefererName = serviceRefererName;
             Configuration.BasicAuthUserName = basicAuthUserName;
             Configuration.BasicAuthPassword = basicAuthPassword;
         }
